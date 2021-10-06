b8d62235113ea169bb12f21133e82143086f5466
johang0128 <jgaleza28@gmail.com>
2021-10-05 21:05:35 -0400

    Initial commit

---
+++ src/Lab0/Print/Main.java
@@ -0,0 +1,12 @@
+package Lab0.Print;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first message");
+        System.out.println("This is my second message");
+        System.out.println("This is my third message");
+        System.out.println("This is another message");
+        System.out.println("This is a message without error");
+    }
+}
